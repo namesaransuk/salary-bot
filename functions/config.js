0d599ce2c444a9d6a9b4bd84d155105ea32125d9
namesaransuk <49622873+namesaransuk@users.noreply.github.com>
2022-10-15 21:28:25 +0700

    Update salary-bot success

---
+++ functions/config.js
@@ -0,0 +1,20 @@
+const lineCredential = {
+  ACCESS_TOKEN:
+    "OU/yGqylSjiV+I0HQ8C4Zdcx26/Np882sHhGrggEmaUpHYPvgCenjqvjMgI8RuvTbnyTqGVLIsqdtMHppoHVpTfxumanWPJgJCttsX4rbOnoanaoSKWBK2JA0BcCu9A+EWqU0UHPoX1GgumQsPbBCwdB04t89/1O/w1cDnyilFU=",
+};
+
+const googleSheetCredential = {
+  SPREADSHEET_ID: "1oRi7Uj2DVUHXBcKHgFhLgk6xWo0yFhmnn5aaAm2MpkU",
+  GOOGLE_SHEET: "salary",
+  RANGE: "A2:E",
+};
+
+const firebaseCredential = {
+  DATABASE_URL: "https://employee-salary-slip-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
+
+module.exports = {
+  lineCredential,
+  googleSheetCredential,
+  firebaseCredential,
+};
